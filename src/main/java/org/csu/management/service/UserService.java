0553f65569e545cf3d2c5794bbcf858dbecf588e
Aier233 <93068627+Aier233@users.noreply.github.com>
2022-03-13 21:03:45 +0800

    加入了Accounts的管理（基础版）

---
+++ src/main/java/org/csu/management/service/UserService.java
@@ -0,0 +1,37 @@
+package org.csu.management.service;
+
+import org.csu.management.domain.User;
+import org.csu.management.persistence.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    public User findUserByUsername(String username){
+        return userMapper.findUserByUsername(username);
+    }
+
+    public boolean updateUserByUsername(User user){
+        return userMapper.updateUserByUsername(user);
+    }
+
+    public User signin(User user){
+        return userMapper.findUserByUsernameAndPassword(user);
+    }
+
+    public boolean register(User user){
+        return userMapper.insertUserByUsernameAndPassword(user);
+    }
+
+    public List<User>searchUserList() {
+        return userMapper.searchUserList();
+    }
+
+}
