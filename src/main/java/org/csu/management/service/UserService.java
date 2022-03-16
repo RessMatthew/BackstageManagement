dd5a1db8819200d1249733b04119f44fa8cf19e0
Aier233 <93068627+Aier233@users.noreply.github.com>
2022-03-16 17:10:52 +0800

    一点点更新，Account管理适配密码MD5值

--- src/main/java/org/csu/management/service/UserService.java
+++ src/main/java/org/csu/management/service/UserService.java
@@ -2,6 +2,7 @@ package org.csu.management.service;
 
 import org.csu.management.domain.User;
 import org.csu.management.mapper.UserMapper;
+import org.csu.management.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +14,20 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    //MD5 加密所用的盐
+    private static final String salt="1a2b3c4d";
 
     public User findUserByUsername(String username){
         return userMapper.findUserByUsername(username);
     }
 
     public boolean updateUserByUsername(User user){
+        String inputPassword = user.getPassword();
+        String MD5Password = MD5Util.inputPassToDBPass(inputPassword,salt);
+        System.out.println("*******************************************");
+        System.out.println("输入密码： "+inputPassword+"  MD5加密后： "+MD5Password);
+        System.out.println("*******************************************");
+        user.setPassword(MD5Password);
         return userMapper.updateUserByUsername(user);
     }
 
